0c6adad8c6c4d487bc43da67059d45a6704e1b52
ss8u19 <ss8u19@soton.ac.uk>
2020-07-16 22:58:09 +0200

    Updates

---
+++ precache-manifest.5b33962996d724b5bd7cf93179bb1254.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15f861ee9d36fafbdfa225fcf453bc63",
+    "url": "/podify/index.html"
+  },
+  {
+    "revision": "5f613c092523cb4143ae",
+    "url": "/podify/static/css/main.087c6b59.chunk.css"
+  },
+  {
+    "revision": "ce6560adc7c1acd8db21",
+    "url": "/podify/static/js/2.92fa8730.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/podify/static/js/2.92fa8730.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f613c092523cb4143ae",
+    "url": "/podify/static/js/main.a64084bb.chunk.js"
+  },
+  {
+    "revision": "2929b171fd902d2dfc2f",
+    "url": "/podify/static/js/runtime-main.f9ba3ae6.js"
+  }
+]);
